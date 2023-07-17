bc351128cd3664da6d6a6e969f38105365b33029
James-TaeyoungSon <139704642+James-TaeyoungSon@users.noreply.github.com>
2023-07-17 21:58:29 +0900

    delete

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import DateField, SubmitField
-from wtforms.validators import DataRequired
-
-class DateForm(FlaskForm):
-    start_date = DateField("시작일을 입력하세요 (예: 2023-07-10):", validators=[DataRequired()])
-    end_date = DateField("종료일을 입력하세요 (예: 2023-07-16):", validators=[DataRequired()])
-    submit = SubmitField("검색")
