ed2e2db5cfacd2e5085671a1ccfd338133430d1c
James-TaeyoungSon <139704642+James-TaeyoungSon@users.noreply.github.com>
2023-07-17 21:55:21 +0900

    third commit

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from bs4 import BeautifulSoup
-import urllib.request
+from flask import Flask, render_template, request
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
-import openpyxl
-from flask import Flask, request, send_file
-from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route("/netflix", methods=["POST"])
-def netflix():
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
+        data = get_netflix_data(start_date, end_date)
+        data_html = data.to_html(index=False)
+
+        return render_template("index.html", data=data_html)
+    return render_template("index.html")
+
+def get_netflix_data(start_date, end_date):
     url = "https://flixpatrol.com/top10/streaming/world/history/"
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html, 'html.parser')
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    start_date = request.form.get("start_date")
-    end_date = request.form.get("end_date")
-
-    neflix = soup.select("body > div > div > table > tbody > tr > td > div > a")
+    neflix_links = soup.select("body > div > div > table > tbody > tr > td > div > a")
 
     result_df = pd.DataFrame(columns=['Date', 'Rank', 'Title', 'Score'])
-
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'}
 
-    for link in neflix:
+    for link in neflix_links:
         url = link.get("href")
         date = url.split('/')[-2]
 
         if url and 'top10/netflix/world' in url and start_date <= date <= end_date:
             url = f'https://flixpatrol.com{url}'
-
             response = requests.get(url, headers=headers)
             response.raise_for_status()
-
             soup = BeautifulSoup(response.text, 'html.parser')
 
             tv_show_section = soup.select_one("#netflix-2")
@@ -45,13 +47,7 @@ def netflix():
             temp_df = pd.DataFrame({'Date': [date] * len(ranks), 'Rank': ranks, 'Title': titles, 'Score': scores})
             result_df = result_df.append(temp_df, ignore_index=True)
 
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='openpyxl')
-    result_df.to_excel(writer, index=False)
-    writer.save()
-
-    output.seek(0)
-    return send_file(output, as_attachment=True, attachment_filename=f"netflix_{start_date}_{end_date}.xlsx", mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    return result_df
 
 if __name__ == "__main__":
     app.run(debug=True)
